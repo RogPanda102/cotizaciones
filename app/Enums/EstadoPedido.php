6701043e7e7f30a41a1a313ca484160089502be8
Rogelio HDZ S <PandaWok17@proton.me>
2026-05-04 11:10:54 -0600

    fix/pedido: error en el enum

--- app/Enums/EstadoPedido.php
+++ app/Enums/EstadoPedido.php
@@ -19,4 +19,46 @@ enum EstadoPedido: string
             
         };
     }
+    public function siguientesEstados(): array
+    {
+        return match($this) {
+            self::EN_PROCESO => [self::ENTREGADO],
+            self::ENTREGADO  => [self::FACTURADO],
+            self::FACTURADO  => [self::PAGADO],
+            self::PAGADO     => [],
+        };
+    }
+    public function puedeCambiarA(self $nuevoEstado): bool
+    {
+        return in_array($nuevoEstado, $this->siguientesEstados(), true);
+    }
+    public function badge(): string
+    {
+        return match($this) {
+            self::EN_PROCESO => 'secondary',
+            self::FACTURADO => 'primary',
+            self::ENTREGADO => 'warning',
+            self::PAGADO => 'success',
+        };
+    }
+    public function esFinal(): bool
+    {
+        return $this === self::PAGADO;
+    }
+    public function noEsFinal(): bool
+    {
+        return !$this->esFinal();
+    }
+    public function requiereFechaFacturacion(): bool
+    {
+        return $this === self::FACTURADO;
+    }
+    public function bloqueaCompras(): bool
+    {
+        return in_array($this, [
+            self::FACTURADO,
+            self::ENTREGADO,
+            self::PAGADO
+        ]);
+    }
 }
