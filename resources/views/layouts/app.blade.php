db78c80b9ffa3711bf0f60bb8e9fdd87bd6f0cd0
mcbryan01 <116581245+mcbryan01@users.noreply.github.com>
2026-05-08 10:56:11 -0600

    Se mandan creacion de js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,6 +129,10 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/additional-methods.min.js"></script>
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
